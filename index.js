13c50e26b5396b063b779b22f6027ffc8e9f7912
namansaroha02 <92263802+namansaroha02@users.noreply.github.com>
2022-07-01 21:33:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 
 // <=====================NAVBAR===================>
-var noOfElem=document.querySelectorAll("li").length;
-const list=document.querySelectorAll(".nav-li");
+var noOfElem = document.querySelectorAll("li").length;
+const list = document.querySelectorAll(".nav-li");
 
-function activeLink(){
-    list.forEach((item)=>
+function activeLink() {
+    list.forEach((item) =>
         item.classList.remove("active"));
     this.classList.add("active");
 }
-list.forEach((item)=>
-    item.addEventListener("click",activeLink));
+list.forEach((item) =>
+    item.addEventListener("click", activeLink));
 
 
 // <=====================SOCIAL=====================>
-document.querySelector(".message").addEventListener("click",function(){
+document.querySelector(".message").addEventListener("click", function () {
     document.querySelector(".msgarea").classList.add("clicked-msg");
 })
 
-document.querySelector(".mail").addEventListener("click",function(){
+document.querySelector(".mail").addEventListener("click", function () {
     document.querySelector(".mailarea").classList.add("clicked-mail");
     // this.classList.add("clicked-mail");
     // this.classList.remove("mailarea");
 })
+
+
+// <===========================MOBILE========================>
+
+document.querySelector("#check").addEventListener("click",function (){
+        document.body.classList.toggle("stop-scroll");
+});
+
+
